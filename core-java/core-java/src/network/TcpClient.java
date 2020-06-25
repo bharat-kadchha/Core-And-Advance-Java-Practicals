f98467a2ec650d9b99cf01386af68d8945505963
bharat-kadchha <bharatkadchha1@gmail.com>
2020-06-25 19:18:10 +0530

    TCP client code added

---
+++ core-java/core-java/src/network/TcpClient.java
@@ -0,0 +1,27 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TcpClient {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		Socket socket = new Socket("localhost", 2222);
+		DataOutputStream dataOutStream = new DataOutputStream(socket.getOutputStream());
+		System.out.println("client say :");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String data = br.readLine();
+		dataOutStream.writeUTF(data);
+
+		dataOutStream.close();
+		socket.close();
+		br.close();
+
+	}
+	
+}
